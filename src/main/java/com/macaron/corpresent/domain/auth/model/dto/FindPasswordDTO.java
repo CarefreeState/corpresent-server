7ab6b88a9de858b6c19e3fdc2be5b44eb0f40f6d
s:103 <2040484356@qq.com>
2024-11-14 17:56:45 +0800

    feat: 用户数据更新相关的接口

--- src/main/java/com/macaron/corpresent/domain/auth/model/dto/FindPasswordDTO.java
+++ src/main/java/com/macaron/corpresent/domain/auth/model/dto/FindPasswordDTO.java
@@ -21,8 +21,8 @@ public class FindPasswordDTO {
     @Schema(description = "邮箱")
     private String email;
 
-    @NotBlank(message = "密码不能为空")
-    @Schema(description = "密码")
+    @NotBlank(message = "新密码不能为空")
+    @Schema(description = "新密码")
     private String password;
 
     @NotBlank(message = "邮箱验证码不能为空")
